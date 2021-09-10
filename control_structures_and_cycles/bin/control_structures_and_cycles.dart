1aaa53ede9d6dd23718bb63f907ee08f1d1f3ec1
Steveen <steveen.echeverri@gmail.com>
2021-09-10 00:31:32 -0500

    If Else and logical operators

--- control_structures_and_cycles/bin/control_structures_and_cycles.dart
+++ control_structures_and_cycles/bin/control_structures_and_cycles.dart
@@ -8,6 +8,8 @@ void main(List<String> arguments) {
   // ignore: omit_local_variable_types
   List<int> integerList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  print(integerList);
+
   integerList
     ..clear()
     ..add(10)
@@ -16,4 +18,21 @@ void main(List<String> arguments) {
     ..insert(1, 0);
 
     print('New list: $integerList');
+
+    // If Else and logical operators AND && OR
+
+    // ignore: omit_local_variable_types
+    bool access = true;
+    int cost = 15300;
+    String coupon = 'Free';
+
+  if((access && cost == 15300) || coupon == 'Free') {
+    if(cost >= 15000) {
+      print('Spare change');
+    }
+    print('Welcome to the course');
+  // ignore: dead_code
+  } else {
+    print('You do not have access');
+  }
 }
